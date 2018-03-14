bacc1a80266686e7527eb0c84ade72825de78a70
Arthur Dassier <arthur.dassier@epitech.eu>
2018-03-14 14:00:32 +0100

    can now change keys

--- srcs/modif_debug.c
+++ srcs/modif_debug.c
@@ -7,28 +7,63 @@
 
 #include "tetris.h"
 
-int get_argv(int oc)
+char *change_str(char *arg, char *printer)
+{
+	int	i = 0;
+	int	j = 0;
+	char	*tmp = malloc(sizeof(char) * 1024);
+
+	while (printer[i] != ':') {
+		tmp[i] = printer[i];
+		++i;
+	}
+	tmp[i] = printer[i];
+	++i;
+	tmp[i] = printer[i];
+	++i;
+	tmp[i] = printer[i];
+	while (arg[j] != '\0') {
+		tmp[i] = arg[j];
+		++i;
+		tmp[i] = '\0';
+		my_realloc(tmp);
+		++j;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
+
+int get_argv(int oc, char_arg *ptb)
 {
 	switch (oc) {
 		case 'D':
 			break;
 		case 'L':
+			ptb->key_level = change_str(optarg, ptb->key_level);
 			break;
 		case 'l':
+			ptb->key_left = change_str(optarg, ptb->key_left);
 			break;
 		case 'r':
+			ptb->key_right = change_str(optarg, ptb->key_right);
 			break;
 		case 't':
+			ptb->key_turn = change_str(optarg, ptb->key_turn);
 			break;
 		case 'd':
+			ptb->key_drop = change_str(optarg, ptb->key_drop);
 			break;
 		case 'q':
+			ptb->key_quit = change_str(optarg, ptb->key_quit);
 			break;
 		case 'p':
+			ptb->key_pause = change_str(optarg, ptb->key_pause);
 			break;
 		case 'm':
+			ptb->key_next = change_str(optarg, ptb->key_next);
 			break;
 		case 'w':
+			ptb->key_size = change_str(optarg, ptb->key_size);
 			break;
 		case ':':
 			my_puterror("read --help before testing random"
@@ -60,7 +95,7 @@ int modif_debug(int ac, char **av, char_arg *printable)
 	};
 	optind = 1;
 	while ((oc = getopt_long(ac, av, "DL:l:r:t:d:q:p:m:w:", longopts, NULL)) != -1)
-		if (get_argv(oc) == 84)
+		if (get_argv(oc, printable) == 84)
 			return (84);
 	return (0);
 }
